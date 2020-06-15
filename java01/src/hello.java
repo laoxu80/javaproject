3ff0df744add2cd7dc8d6c4f69d40404ca0858c4
laoxu80 <xuli.21@163.com>
2020-06-15 15:24:47 +0800

    2020-06-15 创建java项目

---
+++ java01/src/hello.java
@@ -0,0 +1,23 @@
+/*
+多行注释
+多行注释
+*/
+public class hello{ // 单行注释
+	public static void main(String[] args){
+		//字符串常量
+		System.out.println("hello world,hello world");
+		System.out.println("");
+		System.out.println("ABC");
+		//整数常量
+		System.out.println(30);
+		System.out.println(-38);
+		//浮点数常量
+		System.out.println(3.14);
+		System.out.println(-2.98);
+		//字符常量
+		System.out.println('a');
+		System.out.println('6');
+		//System.out.println(3);
+
+	}
+}
