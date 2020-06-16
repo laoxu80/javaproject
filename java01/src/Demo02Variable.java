6f75ae6c4b88845b14f159f6d47a4129e39810ed
laoxu80 <xuli.21@163.com>
2020-06-16 11:09:03 +0800

    2020-06-16 java学习

---
+++ java01/src/Demo02Variable.java
@@ -0,0 +1,24 @@
+public class Demo02Variable {
+    public static void main(String[]args){
+        //创建一个变量
+        int num1;
+        num1=5;
+        System.out.println(num1);
+        num1=20;
+        System.out.println(num1);
+        int num2 = 19;
+        System.out.println(num2+num1);
+        byte num3 = 127;
+        System.out.println(num3);
+        float num4 = 2.5F;
+        long num5 = 30000000000000000L;
+
+        System.out.println(num4);
+        char zifu1 ='中';
+        System.out.println(zifu1);
+        boolean var1 = true;
+        System.out.println(var1);
+        boolean var2 = var1;
+        System.out.println(var2);
+    }
+}
---
+++ java01/src/Demo03.java
@@ -0,0 +1,21 @@
+public class Demo03 {
+    public static void main(String[] args){
+        int num1 =10;
+        //int num1 =20;
+        int a=10,b=20,c=30;
+        System.out.println(a+b+c);
+        long num2=100;
+        System.out.println(num2);
+        // 强制类型转换
+        int num3 =30000;
+        System.out.println(num3);
+        char zifu1= '1';
+        System.out.println(zifu1);
+        char zifu3='c';
+        int num = zifu3;
+        System.out.println(num);
+
+
+
+    }
+}
