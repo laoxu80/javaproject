4464311760a6a484d6a6fd74711a86b3e95a7eea
laoxu80 <xuli.21@163.com>
2020-06-17 16:12:38 +0800

    2020-06-17 条件控制语句学习

--- java01/src/Demo11902Control.java
+++ java01/src/Demo11902Control.java
@@ -1,9 +1,16 @@
 public class Demo11902Control {
     public static void main(String []args){
         int num1=10;
-        if(num1>20){
+        int num2=30;
+        if(num1>num2){
             System.out.println(num1);
         }
-        System.out.println("判断失败");
+        else if(num1==num2)
+        {
+            System.out.println("相等");
+        }
+        else {
+            System.out.println(num2);
+        }
     }
 }
