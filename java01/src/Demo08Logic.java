b196c1008bca6e92c54db88efc6fbed5e2437d91
laoxu80 <xuli.21@163.com>
2020-06-17 14:00:31 +0800

    2020-06-17 运算符学习

---
+++ java01/src/Demo08Logic.java
@@ -0,0 +1,17 @@
+
+public class Demo08Logic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(20>10&&20<30);
+		int a=10;
+		int b=20;
+		int max=a>b?a:b;
+		System.out.println(max);
+
+
+
+
+	}
+
+}
---
+++ java01/src/Demo09Method.java
@@ -0,0 +1,28 @@
+public class Demo09Method {
+    public static void main(String[] args) {
+        farmer();
+        cooker();
+        me();
+    }
+
+    public static void farmer() {
+        System.out.println("播种");
+        System.out.println("施肥");
+        System.out.println("收割");
+        System.out.println("出售");
+        System.out.println("吆喝");
+
+    }
+
+    public static void cooker() {
+        System.out.println("炒菜");
+        System.out.println("切菜");
+        System.out.println("装盘");
+    }
+
+    public static void me() {
+        System.out.println("吃");
+
+        System.out.println("付钱");
+    }
+}
