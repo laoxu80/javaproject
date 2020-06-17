35fced46dce37560e1e5d4e606950d02336092c0
laoxu80 <xuli.21@163.com>
2020-06-17 17:28:37 +0800

    2020-06-17 条件控制语句学习

--- java01/src/Demo11903Control.java
+++ java01/src/Demo11903Control.java
@@ -1,5 +1,24 @@
 public class Demo11903Control {
     public static void main(String[] args) {
+        int score = 12;
+        if(score>100 ||score<0){
+            System.out.println("wrong");
+        }
+        else if(score>=90){
+            System.out.println("优秀");
+        }
+        else if(score>=80){
+            System.out.println("好");
+        }
+        else if(score>=70){
+            System.out.println("良");
+        }
+        else if(score>=60){
+            System.out.println("及格");
+        }
+        else {
+            System.out.println("不及格");
+        }
 
     }
 }
