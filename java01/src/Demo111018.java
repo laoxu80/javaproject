2413ede95df03d0d2640afd290253f206fd4bd46
laoxu80 <xuli.21@163.com>
2020-06-18 17:31:25 +0800

    2020-06-18 for循环学习

--- java01/src/Demo111018.java
+++ java01/src/Demo111018.java
@@ -1,5 +1,10 @@
 public class Demo111018 {
     public static void main(String[] args) {
+        for (int hour=0;hour<24;hour++){
+            for (int minute=0;minute<60;minute++){
+                System.out.println(hour+"点"+minute+"分");
+            }
+        }
 
     }
 }
