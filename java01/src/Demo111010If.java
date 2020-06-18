12050615bfa4d500c462a057cb826db9b5cfc903
laoxu80 <xuli.21@163.com>
2020-06-18 11:31:39 +0800

    2020-06-18 if循环语句学习

---
+++ java01/src/Demo111010If.java
@@ -0,0 +1,5 @@
+public class Demo111010If {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java01/src/Demo11907Control.java
@@ -0,0 +1,12 @@
+public class Demo11907Control {
+    public static void main(String[] args) {
+        swi01();
+    }
+
+    public static void swi01(){
+        int num2=20;
+
+        System.out.println(num2);
+
+    }
+}
