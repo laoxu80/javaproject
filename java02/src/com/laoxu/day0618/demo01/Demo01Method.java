9f243cbfdebff31f337e2ef922b2ea1c34d93698
laoxu80 <xuli.21@163.com>
2020-06-19 10:50:21 +0800

    2020-06-18 idea学习

--- java02/src/com/laoxu/day0618/demo01/Demo01Method.java
+++ java02/src/com/laoxu/day0618/demo01/Demo01Method.java
@@ -2,13 +2,11 @@ package com.laoxu.day0618.demo01;
 
 public class Demo01Method {
     public static void main(String[] args) {
-        for(int i=1;i<=5;i++){
-            for (int i1 = 0; i1 < 20; i1++) {
-                System.out.println("*");
-
+        for(int i=1;i<=5;i++) {
+            for (int j = 0; j < 20; j++) {
+                System.out.print("*");
             }
-
-
+            System.out.println();
         }
     }
 }
