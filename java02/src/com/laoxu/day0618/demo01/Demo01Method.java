a9d7f3ca10e35d58b18d01058f1b7688b8dc8f9e
laoxu80 <xuli.21@163.com>
2020-06-19 10:40:27 +0800

    2020-06-18 idea学习

---
+++ java02/src/com/laoxu/day0618/demo01/Demo01Method.java
@@ -0,0 +1,14 @@
+package com.laoxu.day0618.demo01;
+
+public class Demo01Method {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for (int i1 = 0; i1 < 20; i1++) {
+                System.out.println("*");
+
+            }
+
+
+        }
+    }
+}
--- java02/src/com/laoxu/day0618/demo02/HelloWorld.java
+++ java02/src/com/laoxu/day0618/demo02/HelloWorld.java
@@ -1,7 +1,13 @@
-package com.day0618;
+package com.laoxu.day0618.demo02;
+import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        Scanner sc=null;
+
+
     }
+
+
 }
